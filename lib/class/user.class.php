c4f8176548853a7da521805accdd85cbc6bb7d4e
moovendhan <vmoovendhan3@gmail.com>
2023-02-16 06:58:32 +0000

    Login using oops

--- lib/class/user.class.php
+++ lib/class/user.class.php
@@ -1,13 +1,10 @@
 <?php
-
-    class user{
+    class user
+    {
         // Signup functions
         public static function signup($username, $password, $email_address, $phone)
         {
-            print("$email_address <br>");
-            print("$username <br>");
-            print("$phone <br>");
-            print("$password <br>");
+            $password = md5($password);
             $conn = db::makeConnection();
             $sql = "INSERT INTO `auth` (`email`, `phone`, `fname`, `password`, `active`)
             VALUES ('$email_address', '$phone', '$username', '$password', '1');";
@@ -18,8 +15,24 @@
                 // echo "Error: " . $sql . "<br>" . $conn->error;
                 $error = $conn->error;
             }
-    
             $conn->close();
             return $error;
+        }
+
+        //Login function
+        public static function login($email_address, $password)
+        {
+            $password = md5($password);
+            $conn = db::makeConnection();
+            $query = "SELECT * FROM `auth` WHERE `email` = '$email_address'";
+            $result = $conn->query($query);
+            if ($result->num_rows > 0) {
+                $row = $result->fetch_assoc();
+                if ($row['password'] == $password) {
+                    return $row;
+                } else {
+                    return false;
+                }
+            }
         }
     }
--- lib/load.php
+++ lib/load.php
@@ -1,22 +1,21 @@
 <?php
-
     //load all files from templates
     function load($page)
     {
         include __DIR__."/../_templates/$page.php";
     }
     ?>
-    <?php
+<?php
     //Including class files
-        include_once __DIR__."/class/mic.class.php";
-        include_once __DIR__."/class/db.class.php";
-        include_once __DIR__."/class/user.class.php";
-        include_once __DIR__."/class/session.class.php";
+    include_once __DIR__."/class/mic.class.php";
+    include_once __DIR__."/class/db.class.php";
+    include_once __DIR__."/class/user.class.php";
+    include_once __DIR__."/class/session.class.php";
     ?>
-    <?php
+<?php
     session::start();
     ?>
-    <?php
+<?php
     //load tittle
     function load_title($title)
     {
